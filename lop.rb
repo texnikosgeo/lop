bb73e7886957ae38e75c64d6364cd72d37b90fcd
texnikosgeo <103029638+texnikosgeo@users.noreply.github.com>
2024-04-04 16:36:05 +0300

    Update lop.rb

--- lop.rb
+++ lop.rb
@@ -4,6 +4,6 @@ puts "give prefix"
 pref = gets.chomp.to_i
 puts "how many do you want"
 ts = gets.chomp.to_i
-ts.times{puts [pref, 6.times.map{rand(6)}.join.chomp].join}
+ts.times{puts [pref, 6.times.map{rand(6)}.join].join}
 puts "press enter to exit"
 exit = gets
