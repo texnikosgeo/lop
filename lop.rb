601ccf5c7053fec28249cce24265b206252780c1
texnikosgeo <103029638+texnikosgeo@users.noreply.github.com>
2024-04-04 16:32:33 +0300

    Create lop.rb

---
+++ lop.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+puts "give prefix"
+pref = gets.chomp.to_i
+puts "how many do you want"
+ts = gets.chomp.to_i
+ts.times{puts [pref, 6.times.map{rand(6)}.join.chomp].join}
+puts "press enter to exit"
+exit = gets
