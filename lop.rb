fc6455d8037dccf355c326955852b71faf47de7d
texnikosgeo <103029638+texnikosgeo@users.noreply.github.com>
2024-04-11 11:25:16 +0300

    Update lop.rb
    add tel com's prefix

--- lop.rb
+++ lop.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
-
-puts "give prefix"
+puts "690 xxx xxxx, 693 xxx xxxx, 699 xxx xxxx NOVA"
+puts "694 xxx xxxx, 695 xxx xxxx VODAFONE"
+puts "697 xxx xxxx, 698 xxx xxxx COSMOTE"
+puts "give prefix 4 numbers."
 pref = gets.chomp.to_i
 puts "how many do you want"
 ts = gets.chomp.to_i
